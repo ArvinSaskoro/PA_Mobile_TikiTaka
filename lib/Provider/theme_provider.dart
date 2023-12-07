c40b37a03680c1f4fa89544f784736fe605f811e
Arvinskr <96719897+ArvinSaskoro@users.noreply.github.com>
2023-12-07 21:55:17 +0800

    add

--- lib/Provider/theme_provider.dart
+++ lib/Provider/theme_provider.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class ThemeProvider extends ChangeNotifier {
@@ -17,14 +19,41 @@ class ThemeProvider extends ChangeNotifier {
 
 final ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
-  primaryColor: Colors.blue, // Ganti dengan warna primer untuk light mode
-  hintColor: Colors.orange, // Ganti dengan warna aksen untuk light mode
-  // Tambahkan atribut lain yang diperlukan untuk light theme
+  primaryColor: Color.fromARGB(255, 29, 72, 106),
+  hintColor: Color.fromARGB(255, 18, 45, 66),
+  // colorScheme: ColorScheme.light(
+  //   background: Colors.white, 
+  // ),
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      color: Colors.black, 
+    ),
+    bodySmall: TextStyle(
+      color: Colors.black, 
+    ),
+  ),
 );
 
 final ThemeData darkTheme = ThemeData(
   brightness: Brightness.dark,
-  primaryColor: Colors.teal, // Ganti dengan warna primer untuk dark mode
-  hintColor: Colors.amber, // Ganti dengan warna aksen untuk dark mode
-  // Tambahkan atribut lain yang diperlukan untuk dark theme
+  primaryColor: Color.fromARGB(255, 29, 72, 106),
+  hintColor: Color.fromARGB(255, 41, 179, 173),
+  // colorScheme: ColorScheme.light(
+  //   background: Colors.white, 
+  // ),
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      color: Colors.white, 
+    ),
+    bodySmall: TextStyle(
+      color: Colors.white, 
+    ),
+  ),
 );
+
+
+
+// WARNA TOSCA      29B3AD Color.fromARGB(255, 41, 179, 173)
+// WARNA Biru Muda  1D486A Color.fromARGB(255, 29, 72, 106)
+// WARNA Biru Tua   122D42 Color.fromARGB(255, 18, 45, 66)
+// WARNA Abu cerah  D9D9D9 Color.fromARGB(255, 217, 217, 217)
